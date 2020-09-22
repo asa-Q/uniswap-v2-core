fe47ec5fa2acaa1b3083ef243944389cf6adcc01
asa-Q <asa.qu@qq.com>
2020-09-22 13:32:47 +0800

    test uniswap interface

---
+++ test/EZZC.spec.ts
@@ -0,0 +1,40 @@
+import chai, { expect } from 'chai'
+import { Contract } from 'ethers'
+import { MaxUint256 } from 'ethers/constants'
+import { bigNumberify, hexlify, keccak256, defaultAbiCoder, toUtf8Bytes } from 'ethers/utils'
+import { solidity, MockProvider, deployContract } from 'ethereum-waffle'
+import { ecsign } from 'ethereumjs-util'
+
+import { expandTo18Decimals, getApprovalDigest } from './shared/utilities'
+
+import EZZC from '../build/EZZC.json'
+
+chai.use(solidity)
+
+const TOTAL_SUPPLY = expandTo18Decimals(10000)
+const TEST_AMOUNT = expandTo18Decimals(10)
+
+describe('EZZC', () => {
+  const provider = new MockProvider({
+    hardfork: 'istanbul',
+    mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
+    gasLimit: 9999999
+  })
+  const [wallet, other] = provider.getWallets()
+
+  let token: Contract
+  beforeEach(async () => {
+    token = await deployContract(wallet, EZZC , [TOTAL_SUPPLY])
+  })
+
+
+   it('name', async () => {
+    const name = await token.name()
+    expect(name).to.eq('Uniswap V2')
+  })
+
+
+
+
+
+})
